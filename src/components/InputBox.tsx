505ebd816f7dd3bd35373cc120411bc031ebf1ce
gahui yeom <ykh4933@naver.com>
2023-05-26 09:29:42 +0900

    InputBox 수정

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -1,10 +1,33 @@
-import * as React from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import { ListItem } from '../types/ListItem.ts'; // ListItem 타입 가져오기
 
-export default function InputBox() {
+interface InputBox {
+  onAddItem: (item: ListItem) => void;
+}
+
+const InputBox: React.FC<InputBox> = ({ onAddItem }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (inputValue) {
+      const newItem: ListItem = {
+        id: Date.now(),
+        text: inputValue,
+      };
+      onAddItem(newItem);
+      setInputValue('');
+    }
+  };
+
   return (
     <Box
       component="form"
@@ -13,11 +36,20 @@ export default function InputBox() {
       }}
       noValidate
       autoComplete="off"
+      onSubmit={handleFormSubmit}
     >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <Button variant="contained" size="large" startIcon={<AddIcon />}>
+      <TextField
+        id="outlined-basic"
+        label="URL"
+        variant="outlined"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <Button variant="contained" size="large" startIcon={<AddIcon />} type="submit">
         ADD
       </Button>
     </Box>
   );
-}
+};
+
+export default InputBox;
