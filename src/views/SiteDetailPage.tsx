1f3481bd13872bb9eaf33bbdbe74d00c346b6b26
gahui yeom <ykh4933@naver.com>
2023-05-28 17:46:13 +0900

    SiteDetailPage 생성

--- src/views/SiteDetailPage.tsx
+++ src/views/SiteDetailPage.tsx
@@ -1,5 +1,42 @@
-const SiteDetailPage = () => {
-  return <div>Site Detail 페이지입니다.</div>;
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import SiteDetailList from '../components/SiteDetailList';
+
+interface WaybackAPIResult {
+  timestamp: string;
+  url: string;
+}
+
+const SiteDetailPage: React.FC = () => {
+  const location = useLocation();
+  const [urlList, setUrlList] = useState<WaybackAPIResult[]>([]);
+
+  useEffect(() => {
+    const fetchURLs = async () => {
+      try {
+        const response = await fetch(
+          `https://archive.org/wayback/available?url=${location.state.url}`,
+        );
+        const data = await response.json();
+        setUrlList([data?.archived_snapshots?.closest] || []);
+      } catch (error) {
+        console.error('Error fetching URLs from Wayback API:', error);
+      }
+    };
+
+    fetchURLs();
+  }, []);
+
+  const handleUrlClick = () => {
+    console.log('handleUrlClick');
+  };
+
+  return (
+    <>
+      <div>Site Detail 페이지입니다.</div>
+      {urlList.length > 0 ? <SiteDetailList items={urlList} /> : <p>No URLs found.</p>}
+    </>
+  );
 };
 
 export default SiteDetailPage;
