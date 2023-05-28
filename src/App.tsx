0c7f48a1d3f29c73203561a1a538efc2c37c4f6b
gahui yeom <ykh4933@naver.com>
2023-05-28 12:25:43 +0900

    react-router 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import InputBox from './components/InputBox';
-import TextField from '@mui/material/TextField';
+import SiteRegistration from './components/SiteRegistration';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <InputBox />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SiteRegistration />} />
+        {/* <Route path="/detail/:id" component={DetailPage} /> */}
+      </Routes>
+    </Router>
   );
 }
 
