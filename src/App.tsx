176c16ae501a0b0698fe92d2cb51f9bf423df2ee
gahui yeom <ykh4933@naver.com>
2023-05-29 15:08:26 +0900

    App 파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SiteRegistration from './components/SiteRegistration';
-import SiteDetailPage from './views/SiteDetailPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Container from '@mui/material/Container';
+import Header from './layout/Header';
+import SiteRegisterPage from './views/SiteRegisterPage';
+import SiteDetailPage from './views/SiteDetailPage';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<SiteRegistration />} />
-        <Route path="/detail/:id" element={<SiteDetailPage />} />
-      </Routes>
+      <Container maxWidth="sm">
+        <Header />
+        <Routes>
+          <Route path="/" element={<SiteRegisterPage />} />
+          <Route path="/detail/:id" element={<SiteDetailPage />} />
+        </Routes>
+      </Container>
     </Router>
   );
 }
