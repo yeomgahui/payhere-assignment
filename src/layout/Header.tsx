9ef2e77b0156214470d20d22da3a448b8e3771d8
gahui yeom <ykh4933@naver.com>
2023-05-29 15:03:10 +0900

    Header Component 생성

---
+++ src/layout/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const Header: React.FC = () => {
+  return (
+    <AppBar position="sticky" color="primary">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link to="/" style={{ textDecoration: 'none', color: '#fff' }}>
+            Site History Founder - PAYHEAR
+          </Link>
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
