dfc94663a37e1affbfbc3c3818557a1d97c64081
gahui yeom <ykh4933@naver.com>
2023-05-31 20:07:50 +0900

    header title 수정

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           <Link to="/" style={{ textDecoration: 'none', color: '#fff' }}>
-            Site History Founder - PAYHEAR
+            Site History Finder - PAYHERE
           </Link>
         </Typography>
       </Toolbar>
